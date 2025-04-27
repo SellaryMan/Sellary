89eeca6cd9873807e0d2d83f3637365c9c15da6c
dltjdgh97 <dltjdgh97@daou.co.kr>
2025-04-27 23:00:51 +0900

    feat: 출고상품 조회 api 노출 (#6)

--- sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/in/web/ShippedProductController.kt
+++ sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/in/web/ShippedProductController.kt
@@ -1,17 +1,24 @@
 package org.sellary.sellary.shippedproduct.adapter.`in`.web
 
+import org.sellary.sellary.shippedproduct.application.domain.ShippedProduct
 import org.sellary.sellary.shippedproduct.application.port.dto.ShippedProductCreateCommand
+import org.sellary.sellary.shippedproduct.application.port.dto.ShippedProductDto
+import org.sellary.sellary.shippedproduct.application.port.dto.ShippedProductQuery
 import org.sellary.sellary.shippedproduct.application.port.`in`.CreateShippedProductUseCase
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RestController
+import org.sellary.sellary.shippedproduct.application.port.`in`.ReadShippedProductUseCase
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.*
 
 @RestController("/shipped-product")
 class ShippedProductController(
     private val createUseCase: CreateShippedProductUseCase,
+    private val readUseCase: ReadShippedProductUseCase,
 ) {
     @PostMapping
-    fun createShippedProduct(@RequestBody createShippedProductCommand: ShippedProductCreateCommand) {
+    fun createShippedProduct(@RequestBody createShippedProductCommand: ShippedProductCreateCommand) =
         createUseCase.create(createShippedProductCommand)
-    }
+
+    @GetMapping
+    fun getShippedProduct(@RequestParam query: ShippedProductQuery): List<ShippedProductDto> =
+        readUseCase.readShippedProduct(query)
 }
