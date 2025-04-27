112f43b1ef5cfdab2ca87f253169499efe477ef3
dltjdgh97 <dltjdgh97@daou.co.kr>
2025-04-27 23:01:16 +0900

    refactor: 미사용 import 제거 (#6)

--- sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/in/web/ShippedProductController.kt
+++ sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/in/web/ShippedProductController.kt
@@ -1,12 +1,10 @@
 package org.sellary.sellary.shippedproduct.adapter.`in`.web
 
-import org.sellary.sellary.shippedproduct.application.domain.ShippedProduct
 import org.sellary.sellary.shippedproduct.application.port.dto.ShippedProductCreateCommand
 import org.sellary.sellary.shippedproduct.application.port.dto.ShippedProductDto
 import org.sellary.sellary.shippedproduct.application.port.dto.ShippedProductQuery
 import org.sellary.sellary.shippedproduct.application.port.`in`.CreateShippedProductUseCase
 import org.sellary.sellary.shippedproduct.application.port.`in`.ReadShippedProductUseCase
-import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController("/shipped-product")
