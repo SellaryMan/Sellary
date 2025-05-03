bf22fcea25b600be9e4e113caa3369f3c95a7034
dltjdgh97 <dltjdgh97@daou.co.kr>
2025-05-03 20:11:49 +0900

    fix: 조회 쿼리 join 까지 사용 (#6)

--- sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/out/persistence/ShippedProductJpaRepository.kt
+++ sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/out/persistence/ShippedProductJpaRepository.kt
@@ -3,13 +3,41 @@ package org.sellary.sellary.shippedproduct.adapter.out.persistence
 import org.sellary.sellary.shippedproduct.adapter.out.persistence.entity.ShippedProductEntity
 import org.sellary.sellary.shippedproduct.application.domain.ShippedProductType
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 import org.springframework.transaction.annotation.Transactional
 
 @Repository
 @Transactional(readOnly = true)
 interface ShippedProductJpaRepository : JpaRepository<ShippedProductEntity, Long> {
-    fun findByName(name: String): List<ShippedProductEntity>
-    fun findByType(type: ShippedProductType): List<ShippedProductEntity>
-    fun findByCode(code: String): ShippedProductEntity?
+
+    @Query(
+        """
+       select p from ShippedProductEntity p
+       join fetch p.shippedProductCost
+       join fetch p.shippedProductExpList
+       where p.name = :name
+       """
+    )
+    fun findByNameFetchAll(name: String): List<ShippedProductEntity>
+
+    @Query(
+        """
+       select p from ShippedProductEntity p
+       join fetch p.shippedProductCost
+       join fetch p.shippedProductExpList
+       where p.type = :type
+       """
+    )
+    fun findByTypeFetchAll(type: ShippedProductType): List<ShippedProductEntity>
+
+    @Query(
+        """
+       select p from ShippedProductEntity p
+       join fetch p.shippedProductCost
+       join fetch p.shippedProductExpList
+       where p.code = :code
+       """
+    )
+    fun findByCodeFetchAll(code: String): ShippedProductEntity?
 }
--- sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/out/persistence/ShippedProductQueryJPAPort.kt
+++ sellary-backend/src/main/kotlin/org/sellary/sellary/shippedproduct/adapter/out/persistence/ShippedProductQueryJPAPort.kt
@@ -18,15 +18,16 @@ class ShippedProductQueryJPAPort(
         jpaRepository.findById(id).unwrapOrThrow { "Product with id $id not found" }.toDomain()
 
     @Transactional(readOnly = true)
-    override fun findByName(name: String): List<ShippedProduct> = jpaRepository.findByName(name).map { it.toDomain() }
+    override fun findByName(name: String): List<ShippedProduct> =
+        jpaRepository.findByNameFetchAll(name).map { it.toDomain() }
 
     @Transactional(readOnly = true)
     override fun findByType(type: ShippedProductType): List<ShippedProduct> =
-        jpaRepository.findByType(type).map { it.toDomain() }
+        jpaRepository.findByTypeFetchAll(type).map { it.toDomain() }
 
     @Transactional(readOnly = true)
     override fun findByCode(code: String): ShippedProduct =
-        jpaRepository.findByCode(code).unwrapOrThrow { "Product with code: $code not found" }.toDomain()
+        jpaRepository.findByCodeFetchAll(code).unwrapOrThrow { "Product with code: $code not found" }.toDomain()
 
     @Transactional(readOnly = true)
     override fun findAll(): List<ShippedProduct> = jpaRepository.findAll().map { it.toDomain() }
