214c3779bc004bebf0b08559ce8f4e13b705964d
dltjdgh97 <dltjdgh97@konkuk.ac.kr>
2025-05-05 15:27:49 +0900

    test: 출고상품 jpa test 추가 (#6)

--- sellary-backend/build.gradle.kts
+++ sellary-backend/build.gradle.kts
@@ -47,6 +47,7 @@ dependencies {
     testImplementation("org.jetbrains.kotlin:kotlin-test-junit5")
     testImplementation("org.mockito.kotlin:mockito-kotlin:5.4.0")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    testImplementation("com.h2database:h2")
 
     // JASYPT dependencies
     implementation("com.github.ulisesbocchio:jasypt-spring-boot-starter:3.0.3")
