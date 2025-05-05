6a60583ebbbf184d94cea9007866a269c6e7b8ea
HyeongjinKim98 <201721157@sangmyung.kr>
2025-05-05 21:06:24 +0900

    test : Jest 의존성 / 설정 및 스크립트 파일 추가

---
+++ sellary-frontend/jest.config.js
@@ -0,0 +1,17 @@
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+const customJestConfig = {
+  setupFilesAfterEnv: ['<rootDir>/jest.setup.js'],
+  moduleNameMapper: {
+    '^.+\\.(css|less|scss|sass)$': 'identity-obj-proxy',
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testEnvironment: 'jest-environment-jsdom',
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/.next/'],
+};
+
+module.exports = createJestConfig(customJestConfig);
