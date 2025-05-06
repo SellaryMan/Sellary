c603c1d65c3354109dd708876302fda69a48540d
dltjdgh97 <dltjdgh97@konkuk.ac.kr>
2025-05-06 16:19:47 +0900

    test: get unused warning 제거 (#23)

--- sellary-backend/src/test/kotlin/org/sellary/sellary/autoconfigure/PartialDataJpaTest.kt
+++ sellary-backend/src/test/kotlin/org/sellary/sellary/autoconfigure/PartialDataJpaTest.kt
@@ -16,9 +16,12 @@ import kotlin.reflect.KClass
 @EntityScan
 @EnableJpaRepositories
 annotation class PartialDataJpaTest(
+
+    @Suppress("unused") // 사용되고 있으나, unused 경고를 뱉음. ide 버그로 추정
     @get:AliasFor(annotation = EntityScan::class, attribute = "basePackageClasses")
     val entityPackageClasses: Array<KClass<*>> = [],
 
+    @Suppress("unused") // 사용되고 있으나, unused 경고를 뱉음. ide 버그로 추정
     @get:AliasFor(annotation = EnableJpaRepositories::class, attribute = "basePackageClasses")
     val repositoryPackageClasses: Array<KClass<*>> = []
 )
