31fa57b6b7020a16528e587ba5bd07c76d5decc9
HyeongjinKim98 <201721157@sangmyung.kr>
2025-05-15 17:23:08 +0900

    remove : 불필요 데이터 input 비활성화

--- sellary-frontend/components/outbound/CreateProduct.tsx
+++ sellary-frontend/components/outbound/CreateProduct.tsx
@@ -79,11 +79,11 @@ export const CreateProduct = ()=>{
                     <label htmlFor="company" className="text-left">
                     회사명
                     </label>
-                    <Input id="company" className="col-span-3"/>
+                    <Input disabled id="company" className="col-span-3"/>
                     <label htmlFor="supplier" className="text-left">
                     공급사
                     </label>
-                    <Input id="supplier" className="col-span-3" />
+                    <Input disabled id="supplier" className="col-span-3" />
                     <label htmlFor="category" className="text-left">
                     구분
                     </label>
@@ -93,11 +93,11 @@ export const CreateProduct = ()=>{
                     <label htmlFor="productCode" className="text-left col-span-2">
                     출고상품코드
                     </label>
-                    <Input id="productCode" className="col-span-2" />
+                    <Input id="productCode" className="col-span-4" />
                     <label htmlFor="productName" className="text-left">
                     상품명
                     </label>
-                    <Input id="productName" className="col-span-7" />
+                    <Input id="productName" className="col-span-5" />
                 </div>
                 <div className="grid grid-cols-12 items-center gap-4">
                 <label htmlFor="barcode" className="text-left">
@@ -110,25 +110,25 @@ export const CreateProduct = ()=>{
                     <Input id="keyword" className="col-span-7" />
                 </div>
                 <h2 className="mt-4">규격</h2>
-                <h3>낱개규격</h3>
-                <div className="grid grid-cols-12 items-center gap-4">
+                <h3 >낱개규격</h3>
+                <div className="grid grid-cols-12 h-6 items-center gap-4">
                     <label htmlFor="width" className="text-left">
                     가로
                     </label>
-                    <Input id="width" className="col-span-2" />
+                    <Input disabled id="width" className="col-span-2" />
                     <label htmlFor="length" className="text-left">
                     세로
                     </label>
-                    <Input id="length" className="col-span-2" />
+                    <Input disabled id="length" className="col-span-2" />
                     <label htmlFor="height" className="text-left">
                     높이
                     </label>
-                    <Input id="height" className="col-span-2" /><label htmlFor="weight" className="text-left">
+                    <Input disabled id="height" className="col-span-2" /><label htmlFor="weight" className="text-left ">
                     무게
                     </label>
-                    <Input id="weight" className="col-span-2" />
+                    <Input disabled id="weight" className="col-span-2" />
                 </div>
-                <h3>박스</h3>
+                <h3>박스규격</h3>
                 <div className="grid grid-cols-12 items-center gap-4">
                     <label htmlFor="boxCount" className="text-left col-span-2">
                     박스개수
@@ -143,15 +143,25 @@ export const CreateProduct = ()=>{
                 </div>
                 <div className="grid gap-1 py-4 [&_label,&_input]:text-[0.6rem] [&_label]:min-w-18 [&_input]:h-6 [&_h2]:text-[1rem] [&_h3]:text-[0.8rem]">
                 <h2 className="mt-4">유통기한 관리</h2>
+                <div className="grid grid-cols-12 items-center gap-4">
+                    <label htmlFor="manufactureDate" className="text-left col-span-1">
+                    제조일자
+                    </label>
+                    <Input id="manufactureDate" className="col-span-3" />
+                    <label htmlFor="expDate" className="text-left col-span-1">
+                    유통기한
+                    </label>
+                    <Input id="expDate" className="col-span-3" />
+                </div>
                 <div className="grid grid-cols-15 items-center gap-4">
                     <label htmlFor="lowStockThresholdDay" className="text-left col-span-3">
                     임박재고 전환 기준일
                     </label>
-                    <Input id="lowStockThresholdDay" className="col-span-4" />
+                    <Input id="lowStockThresholdDay" className="col-span-2" />
                     <label htmlFor="noShippingThresholdDay" className="text-left col-span-3">
                     출고불가 기준일
                     </label>
-                    <Input id="noShippingThresholdDay" className="col-span-3" />
+                    <Input id="noShippingThresholdDay" className="col-span-2" />
                 </div>
                 <h2 className="mt-4">비용 관리</h2>
                 <h3>매입가격</h3>
@@ -161,21 +171,21 @@ export const CreateProduct = ()=>{
                     </label>
                     <Input id="unitPurchasePrice" className="col-span-2" />
                     <label className="text-left col-span-2">
-                    개
+                    원
                     </label>
                     <label htmlFor="boxPurchasePrice" className="text-left col-span-1">
                     박스
                     </label>
                     <Input id="boxPurchasePrice" className="col-span-2" />
                     <label className="text-left col-span-2">
-                    개
+                    원
                     </label>
                     <label htmlFor="palettePurchasePrice" className="text-left col-span-1">
                     팔레트
                     </label>
                     <Input id="palettePurchasePrice" className="col-span-2" />
                     <label className="text-left col-span-2">
-                    개
+                    원
                     </label>
                 </div>
                 <h3>판매가격</h3>
@@ -185,21 +195,21 @@ export const CreateProduct = ()=>{
                     </label>
                     <Input id="unitSellingPrice" className="col-span-2" />
                     <label className="text-left col-span-2">
-                    개
+                    원
                     </label>
                     <label htmlFor="boxSellingPrice" className="text-left col-span-1">
                     박스
                     </label>
                     <Input id="boxSellingPrice" className="col-span-2" />
                     <label className="text-left col-span-2">
-                    개
+                    원
                     </label>
                     <label htmlFor="paletteSellingPrice" className="text-left col-span-1">
                     팔레트
                     </label>
                     <Input id="paletteSellingPrice" className="col-span-2" />
-                    <label className="text-left col-span-2">
-                    개
+                    <label className="text-left col-span-1">
+                    원
                     </label>
                 </div>
                 <h2 className="mt-4">보관위치</h2>
@@ -207,12 +217,12 @@ export const CreateProduct = ()=>{
                     <label htmlFor="storage" className="text-left col-span-1">
                     창고
                     </label>
-                    <Input id="storage" className="col-span-4" />
+                    <Input disabled id="storage" className="col-span-4" />
                     
                     <label htmlFor="storageLocation" className="text-left col-span-1">
                     위치
                     </label>
-                    <Input id="storageLocation" className="col-span-4" />
+                    <Input disabled id="storageLocation" className="col-span-4" />
                 </div>
                 </div>
             </div>
