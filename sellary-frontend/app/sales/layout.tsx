10d14a0b0d45b989e67b30fd48f8cfbed7fd66cb
HyeongjinKim98 <201721157@sangmyung.kr>
2025-05-19 07:41:23 +0900

    fix : 미사용 search 컴포넌트 삭제

--- sellary-frontend/app/sales/layout.tsx
+++ sellary-frontend/app/sales/layout.tsx
@@ -1,4 +1,3 @@
-import SearchBox from "@/components/search/SearchBox";
 export default function Layout({
   children,
 }: Readonly<{
@@ -8,7 +7,6 @@ export default function Layout({
     <div className="h-full w-full">
       <div className="text-m p-2">상품관리 &gt; 판매상품</div>
       <div className="h-1/9">
-        <SearchBox/>
       </div>
       <div className="relative mt-4 not-first:h-8/9 w-full overflow-y-auto">
         {children}
