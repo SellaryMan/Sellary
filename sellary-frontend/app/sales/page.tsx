b153598a8f5ec618d7e1e8abcb0752d81574bef8
HyeongjinKim98 <201721157@sangmyung.kr>
2025-05-19 07:43:14 +0900

    remove : 미사용 컴포넌트 관련 코드 삭제

--- sellary-frontend/app/sales/page.tsx
+++ sellary-frontend/app/sales/page.tsx
@@ -1,11 +1,7 @@
-import { DataTable } from "@/components/search/Table";
 import dummyData from "@/components/search/dummy";
 export default function Page() {
   return (
      <>
-     <DataTable
-            className="text-[14px]"
-            data={dummyData}/>
      </>
   );
 }
